e19bf7a39c4ad92faecd894d1fdbf8d04c6dfb79
Umron Alkotob <ulympicstudio@gmail.com>
2026-02-16 17:40:35 -0800

    Add IB_CLIENT_ID_OVERRIDE env var for testing (avoid 'already in use' errors)

--- config/ib_config.py
+++ config/ib_config.py
@@ -1,9 +1,11 @@
 # config/ib_config.py
 # Single source of truth for IB connection settings
 
+import os
+
 HOST = "127.0.0.1"
 PORT = 7497      # Paper TWS port
-CLIENT_ID = 13   # Change if you see "client id already in use"
+CLIENT_ID = int(os.getenv("IB_CLIENT_ID_OVERRIDE", "13"))   # Override with env var if needed
 
 # Backward compatibility aliases (so either naming works)
 IB_HOST = HOST
