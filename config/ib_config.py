2f80e17d30ab19ddb499c0b82a868d9b39c94819
Umron Alkotob <ulympicstudio@gmail.com>
2026-02-16 17:20:19 -0800

    Add backward compatibility aliases for IB config (IB_HOST, IB_PORT, IB_CLIENT_ID)

--- config/ib_config.py
+++ config/ib_config.py
@@ -6,3 +6,6 @@ PORT = 7497      # Paper TWS port
 CLIENT_ID = 13   # Change if you see "client id already in use"
 
 # Backward compatibility aliases (so either naming works)
+IB_HOST = HOST
+IB_PORT = PORT
+IB_CLIENT_ID = CLIENT_ID
